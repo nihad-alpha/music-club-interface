856a4b856b614b8618b945e854001dfd44b93a51
Arman <arman.habul@stu.ibu.edu.ba>
2021-04-28 15:34:43 +0200

    dummy data added

--- index.php
+++ index.php
@@ -7,14 +7,25 @@
 <body>
 
 <div id="header">
-
+<left><img src = "images/dummy-icon.png"></img></left>
 </div>
 
 <div id="sidebar">
 
+<ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Members</li>
+    <li>Comments</li>
+</ul>
+
 </div>
 
 <div id="data">
+    <br>
+    <center><h1>Welcome to the Club!</h1>
+    <p>We hope to keep you :)</p>
+    <p>Sit back and vibe pls!</p></center>
 
 </div>
 
