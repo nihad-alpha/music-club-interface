5477c8e8f3389106596b4891346c4cc640e5c4dd
Arman <arman.habul@stu.ibu.edu.ba>
2021-04-28 15:00:14 +0200

    layout and style basics

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Music Club</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<div id="header">
+
+</div>
+
+<div id="sidebar">
+
+</div>
+
+<div id="data">
+
+</div>
+
+</body>
+
+</html>
